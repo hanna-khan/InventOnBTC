13d06fbb65a35a2f75e0282e65c077264849fb39
Hania Imran <hanna.khan205@gmail.com>
2024-03-21 01:20:02 +0500

    home page wip

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,89 @@
-import { Box, Text } from "@chakra-ui/react";
-import React from "react";
+import * as React from "react";
+import {
+  Container,
+  Heading,
+  Stack,
+  HStack,
+  Text,
+  useColorModeValue,
+  Button,
+  Image,
+  Skeleton,
+  Box,
+  Link,
+} from "@chakra-ui/react";
+import HeroImage from "../../assets/images/hero_img.png";
+import Coin1 from "../../assets/images/coin_1.png";
+import Coin2 from "../../assets/images/coin_2.png";
 
 const Home = () => {
   return (
-    <Box>
-      <Text>Home</Text>
-    </Box>
+    <Stack
+      direction={{ base: "column-reverse", md: "row" }}
+      mt={{ base: 0, md: -1 }}
+      gap={0}
+    >
+      <Stack
+        direction="column"
+        bg="#151517"
+        color="white"
+        p={6}
+        w={{ base: "100%", md: "60%" }}
+      >
+        <Heading as="h3" size="lg" fontWeight="bold" textAlign="left" mb={2}>
+          Crowdfunding for Bitcoin <br /> start-ups
+        </Heading>
+        <Text
+          fontSize="1.2rem"
+          textAlign="left"
+          lineHeight="1.375"
+          fontWeight="300"
+          maxW={{ base: "100%", md: "470px" }}
+          mb={4}
+          color={useColorModeValue("#6d6d6e", "gray.400")}
+        >
+          Join the revolution and support groundbreaking Bitcoin based projects
+        </Text>
+        <HStack spacing={3}>
+          <Button
+            colorScheme="teal"
+            variant="solid"
+            size="md"
+            rounded="md"
+            boxShadow="md"
+            color="black"
+            bg="white"
+          >
+            For Backers
+          </Button>
+          <Button
+            colorScheme="teal"
+            variant="solid"
+            size="md"
+            rounded="md"
+            boxShadow="md"
+            color="black"
+            bg="white"
+          >
+            For Starters
+          </Button>
+        </HStack>
+        <HStack spacing={5} mt={4}>
+          <Image sx={{ width: "3vw" }} src={Coin1} alt="Coin1" />
+          <Image sx={{ width: "3vw" }} src={Coin2} alt="Coin2" />
+        </HStack>
+      </Stack>
+      <Box w={{ base: "100%", md: "40vw" }}>
+        <Image
+          w="100%"
+          h="100%"
+          minW={{ base: "auto", md: "30rem" }}
+          objectFit="cover"
+          src={HeroImage}
+          fallback={<Skeleton />}
+        />
+      </Box>
+    </Stack>
   );
 };
 
