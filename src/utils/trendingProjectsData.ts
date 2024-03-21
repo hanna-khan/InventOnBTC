254c26c68865cdb260396ff2edea1dd125888c7e
Hania Imran <hanna.khan205@gmail.com>
2024-03-22 01:23:03 +0500

    trending projects fix

---
+++ src/utils/trendingProjectsData.ts
@@ -0,0 +1,55 @@
+import image1 from '../assets/images/project_1.png';
+import image2 from '../assets/images/project_2.png';
+import image3 from '../assets/images/project_3.png';
+import image4 from '../assets/images/project_4.png';
+
+export const data = [
+  {
+    image: image1,
+    title: "Funding",
+    subTitle: "The world in shambles",
+    content:
+      "Step into a post apocalyptic world where only Bitcoin can save the people",
+    subContent: "Bitcoin Metaverse",
+    time: "24 days left",
+    barRate: "23000",
+    barTitle: "STC raised",
+    barPercent: "23%",
+  },
+  {
+    image: image2,
+    title: "Funding",
+    subTitle: "The world in shambles",
+    content:
+      "Step into a post apocalyptic world where only Bitcoin can save the people",
+    subContent: "Bitcoin Metaverse",
+    time: "24 days left",
+    barRate: "23000",
+    barTitle: "STC raised",
+    barPercent: "23%",
+  },
+  {
+    image: image3,
+    title: "Funding",
+    subTitle: "The world in shambles",
+    content:
+      "Step into a post apocalyptic world where only Bitcoin can save the people",
+    subContent: "Bitcoin Metaverse",
+    time: "24 days left",
+    barRate: "23000",
+    barTitle: "STC raised",
+    barPercent: "23%",
+  },
+  {
+    image: image4,
+    title: "Funding",
+    subTitle: "The world in shambles",
+    content:
+      "Step into a post apocalyptic world where only Bitcoin can save the people",
+    subContent: "Bitcoin Metaverse",
+    time: "24 days left",
+    barRate: "23000",
+    barTitle: "STC raised",
+    barPercent: "23%",
+  },
+];
