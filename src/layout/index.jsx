2b2be8871838cc2e72ee2bb41c5f57d7b89c1ab1
Hania Imran <hanna.khan205@gmail.com>
2024-03-24 01:56:30 +0500

    fixes

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -6,9 +6,9 @@ import { Outlet } from "react-router-dom";
 const Layout = () => {
   return (
     <Flex sx={{ flexDirection: "column", height: "100vh" }}>
-      <Header />
+      {/* <Header /> */}
       <Outlet />
-      <Footer />
+      {/* <Footer /> */}
     </Flex>
   );
 };
