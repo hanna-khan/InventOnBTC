a318bc3b4adea82b7c47466637c2c52e9f1c9c6c
Hania Imran <hanna.khan205@gmail.com>
2024-03-23 16:10:42 +0500

    footer wip

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -10,9 +10,11 @@ import {
   Input,
   Checkbox,
   Button,
+  SimpleGrid,
 } from "@chakra-ui/react";
 import FooterLogo from "../../assets/images/Footer_logo.png";
 import { BsEnvelope } from "react-icons/bs";
+import { FaDiscord, FaYoutube, FaTwitter, FaFacebookF } from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -24,144 +26,160 @@ const Footer = () => {
       py="2.5rem"
       fontSize="0.875rem"
     >
-      <Box
-        maxW="64rem"
-        marginX="auto"
-        pb="2rem"
-        mb="1.5rem"
+      <SimpleGrid
+        columns={{ base: 1, sm: 2, md: 3, lg: 5 }}
+        gap={6}
         px={10}
-        borderBottom="1px solid"
-        borderColor="gray.300"
       >
-        <Flex flexWrap="wrap" alignItems="start" justifyContent="space-between">
-          <Box
-            w={{ base: "100%", sm: "50%", md: "max-content" }}
-            mb={{ base: "1.5rem", lg: "0" }}
+        <Flex flexDir="column">
+          <Image src={FooterLogo} alt="Coin1" width={"5vw"} />
+        </Flex>
+        <Flex flexDir="column">
+          <Heading
+            as="h5"
+            color="gray.600"
+            mb="0.5rem"
+            fontSize="0.875rem"
+            fontWeight="600"
           >
-            <Image sx={{ width: "5vw" }} src={FooterLogo} alt="Coin1" />
-          </Box>
-          <Box
-            w={{ base: "100%", sm: "50%", md: "max-content" }}
-            mb={{ base: "1.5rem", lg: "0" }}
+            EXPLORE
+          </Heading>
+          <List lineHeight="2" justifyContent="center">
+            <LinkItem text="What we do" />
+            <LinkItem text="Funding" />
+          </List>
+        </Flex>
+        <Flex flexDir="column">
+          <Heading
+            as="h5"
+            color="gray.600"
+            mb="0.5rem"
+            fontSize="0.875rem"
+            fontWeight="600"
           >
-            <Heading
-              as="h5"
-              color="gray.600"
-              mb="0.5rem"
-              fontSize="0.875rem"
-              fontWeight="600"
-            >
-              EXPLORE
-            </Heading>
-            <List lineHeight="2" justifyContent="center">
-              <LinkItem text="What we do" />
-              <LinkItem text="Funding" />
-            </List>
-          </Box>
-          <Box
-            w={{ base: "100%", sm: "50%", md: "max-content" }}
-            mb={{ base: "1.5rem", lg: "0" }}
+            ABOUT
+          </Heading>
+          <List lineHeight="2">
+            <LinkItem text="About us &amp; Safety" />
+            <LinkItem text="Blog" />
+            <LinkItem text="Trust & Safety" />
+            <LinkItem text="Help & Support" />
+            <LinkItem text="Press" />
+            <LinkItem text="Careers" />
+            <LinkItem text="Contact" />
+          </List>
+        </Flex>
+        <Flex flexDir="column">
+          <Heading
+            as="h5"
+            color="gray.600"
+            mb="0.5rem"
+            fontSize="0.875rem"
+            fontWeight="600"
           >
-            <Heading
-              as="h5"
-              color="gray.600"
-              mb="0.5rem"
-              fontSize="0.875rem"
-              fontWeight="600"
-            >
-              ABOUT
-            </Heading>
-            <List lineHeight="2">
-              <LinkItem text="About us &amp; Safety" />
-              <LinkItem text="Blog" />
-              <LinkItem text="Trust & Safety" />
-              <LinkItem text="Help & Support" />
-              <LinkItem text="Press" />
-              <LinkItem text="Careers" />
-              <LinkItem text="Contact" />
-            </List>
-          </Box>
-          <Box
-            w={{ base: "100%", sm: "50%", md: "max-content" }}
-            mb={{ base: "1.5rem", lg: "0" }}
+            Entrepreneurs
+          </Heading>
+          <List lineHeight="2">
+            <LinkItem text="How it works" />
+            <LinkItem text="Creator Corner" />
+            <LinkItem text="Fees" />
+          </List>
+        </Flex>
+        <Flex flexDir="column" align="center">
+          <BsEnvelope size={30} />
+          <Heading
+            as="h5"
+            color="#000"
+            mb="0.5rem"
+            fontSize="0.875rem"
+            fontWeight="600"
+            textAlign="center"
           >
-            <Heading
-              as="h5"
-              color="gray.600"
-              mb="0.5rem"
-              fontSize="0.875rem"
-              fontWeight="600"
-            >
-              Entrepreneurs
-            </Heading>
-            <List lineHeight="2">
-              <LinkItem text="How it works" />
-              <LinkItem text="Creator Corner" />
-              <LinkItem text="Fees" />
-            </List>
-          </Box>
-          <Box
-            w={{ base: "100%", sm: "50%", md: "max-content" }}
-            mb={{ base: "1.5rem", lg: "0" }}
-            display={"flex"}
-            flexDir={"column"}
-            alignItems={"center"}
-            gap={2}
+            Get it first
+          </Heading>
+          <Text textAlign="center">
+            Discover new innovations in the <br /> InventOnBTC newsletter
+          </Text>
+          <Input
+            placeholder="Your Email address"
+            size="sm"
+            height="40px"
+            width="200px"
+            bg={"white"}
+          />
+          <Checkbox>
+            <p style={{ fontSize: "12px" }}>
+              I agree to the{" "}
+              <span style={{ color: "#fba418" }}>Terms of Use </span> and have{" "}
+              <br /> read and understand the{" "}
+              <span style={{ color: "#fba418" }}>Privacy Policy</span>
+            </p>
+          </Checkbox>
+          <Button
+            marginTop={4}
+            size="md"
+            height="35px"
+            width="200px"
+            borderColor="#fba418"
+            bg="#fba418"
+            color={"#fff"}
+            rounded={0}
+            textTransform={"uppercase"}
+            fontSize={"12px"}
           >
-            <BsEnvelope size={30} />
-            <Heading
-              as="h5"
-              color="#000"
-              mb="0.5rem"
-              fontSize="0.875rem"
-              fontWeight="600"
-              textAlign={"center"}
-            >
-              Get it first
-            </Heading>
-            <Text textAlign={"center"}>
-              Discover new innovations in the <br /> InventOnBTC newsletter
-            </Text>
-            <Input
-              placeholder="Your Email address"
-              size="sm"
-              height="40px"
-              width="200px"
-              bg={"white"}
-            />
-            <Checkbox>
-              <p style={{ fontSize: "12px" }}>
-                I agree to the{" "}
-                <span style={{ color: "#fba418" }}>Terms of Use </span> and have{" "}
-                <br /> read and understand the{" "}
-                <span style={{ color: "#fba418" }}>Privacy Policy</span>
-              </p>
-            </Checkbox>
-            <Button
-              marginTop={4}
-              size="md"
-              height="35px"
-              width="200px"
-              borderColor="#fba418"
-              bg="#fba418"
-              color={"#fff"}
-              rounded={0}
-              textTransform={"uppercase"}
-              fontSize={"12px"}
-            >
-              Sign Me Up
-            </Button>
-          </Box>
+            Sign Me Up
+          </Button>
+        </Flex>
+      </SimpleGrid>
+      <Box
+        display={"flex"}
+        gap={6}
+        justifyContent={{ base: "flex-start", lg: "center" }}
+      >
+        <Button
+          marginTop={4}
+          size="md"
+          height="35px"
+          width="200px"
+          borderColor="#ccc"
+          bg="#fff"
+          color={"#000"}
+          rounded={0}
+          textTransform={"uppercase"}
+          fontSize={"12px"}
+        >
+          BTC & STX
+        </Button>
+        <Flex gap={4} mt={7} justify="center">
+          <FaFacebookF />
+          <FaTwitter />
+          <FaYoutube />
+          <FaDiscord />
         </Flex>
       </Box>
-      <Flex maxW="64rem" mx="auto" alignItems="center" px={10} flexDir={'row'} justifyContent={'center'}>
-        <List lineHeight="2" display={'flex'} gap={4} flexDir={'row'} >
-          <LinkItem color={'#000'} fontSize={'10px'} text="Terms of Use" />
-          <LinkItem color={'#000'} fontSize={'10px'} text="Privacy Policy" />
-          <LinkItem color={'#000'} fontSize={'10px'} text="Cookie Policy" />
-          <LinkItem color={'#000'} fontSize={'10px'} text="Do Not Sell My Personal Information" />
-          <LinkItem color={'#000'} fontSize={'10px'} text="Accessibility" />
-          <LinkItem color={'#000'} fontSize={'10px'} text="© 2024 InventOnBTC, Inc. All Rights Reserved" />
+      <Flex
+        maxW="64rem"
+        mx="auto"
+        alignItems="center"
+        px={10}
+        justifyContent={"center"}
+        mt={10}
+      >
+        <List lineHeight="2" display={"flex"} gap={4}>
+          <LinkItem color={"#000"} fontSize={"10px"} text="Terms of Use" />
+          <LinkItem color={"#000"} fontSize={"10px"} text="Privacy Policy" />
+          <LinkItem color={"#000"} fontSize={"10px"} text="Cookie Policy" />
+          <LinkItem
+            color={"#000"}
+            fontSize={"10px"}
+            text="Do Not Sell My Personal Information"
+          />
+          <LinkItem color={"#000"} fontSize={"10px"} text="Accessibility" />
+          <LinkItem
+            color={"#000"}
+            fontSize={"10px"}
+            text="© 2024 InventOnBTC, Inc. All Rights Reserved"
+          />
         </List>
       </Flex>
     </Box>
