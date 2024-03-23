e39d3e134c8e8a64550efc3b6ffd86d40a8ac10c
Hania Imran <hanna.khan205@gmail.com>
2024-03-23 15:25:34 +0500

    footer wip

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -7,7 +7,9 @@ import {
   Link,
   Text,
   Image,
-  Input
+  Input,
+  Checkbox,
+  Button,
 } from "@chakra-ui/react";
 import FooterLogo from "../../assets/images/Footer_logo.png";
 import { BsEnvelope } from "react-icons/bs";
@@ -101,30 +103,66 @@ const Footer = () => {
           <Box
             w={{ base: "100%", sm: "50%", md: "max-content" }}
             mb={{ base: "1.5rem", lg: "0" }}
+            display={"flex"}
+            flexDir={"column"}
+            alignItems={"center"}
+            gap={2}
           >
             <BsEnvelope size={30} />
-            <List lineHeight="2">
-              <Text>Get it first</Text>
-              <Text>Discover new innovation
-              <Input placeholder='Your Email address' size='md' /></Text>
-            </List>
+            <Heading
+              as="h5"
+              color="#000"
+              mb="0.5rem"
+              fontSize="0.875rem"
+              fontWeight="600"
+              textAlign={"center"}
+            >
+              Get it first
+            </Heading>
+            <Text textAlign={"center"}>
+              Discover new innovations in the <br /> InventOnBTC newsletter
+            </Text>
+            <Input
+              placeholder="Your Email address"
+              size="sm"
+              height="40px"
+              width="200px"
+              bg={"white"}
+            />
+            <Checkbox>
+              <p style={{ fontSize: "12px" }}>
+                I agree to the{" "}
+                <span style={{ color: "#fba418" }}>Terms of Use </span> and have{" "}
+                <br /> read and understand the{" "}
+                <span style={{ color: "#fba418" }}>Privacy Policy</span>
+              </p>
+            </Checkbox>
+            <Button
+              marginTop={4}
+              size="md"
+              height="35px"
+              width="200px"
+              borderColor="#fba418"
+              bg="#fba418"
+              color={"#fff"}
+              rounded={0}
+              textTransform={"uppercase"}
+              fontSize={"12px"}
+            >
+              Sign Me Up
+            </Button>
           </Box>
         </Flex>
       </Box>
-      <Flex maxW="64rem" mx="auto" alignItems="center" px={10}>
-        <svg
-          fill="#008F94"
-          style={{ width: "1.25rem", height: "1.25rem" }}
-          viewBox="0 0 1000 1000"
-          role="presentation"
-          aria-hidden="true"
-          focusable="false"
-        >
-          <path d="m499.3 736.7c-51-64-81-120.1-91-168.1-10-39-6-70 11-93 18-27 45-40 80-40s62 13 80 40c17 23 21 54 11 93-11 49-41 105-91 168.1zm362.2 43c-7 47-39 86-83 105-85 37-169.1-22-241.1-102 119.1-149.1 141.1-265.1 90-340.2-30-43-73-64-128.1-64-111 0-172.1 94-148.1 203.1 14 59 51 126.1 110 201.1-37 41-72 70-103 88-24 13-47 21-69 23-101 15-180.1-83-144.1-184.1 5-13 15-37 32-74l1-2c55-120.1 122.1-256.1 199.1-407.2l2-5 22-42c17-31 24-45 51-62 13-8 29-12 47-12 36 0 64 21 76 38 6 9 13 21 22 36l21 41 3 6c77 151.1 144.1 287.1 199.1 407.2l1 1 20 46 12 29c9.2 23.1 11.2 46.1 8.2 70.1zm46-90.1c-7-22-19-48-34-79v-1c-71-151.1-137.1-287.1-200.1-409.2l-4-6c-45-92-77-147.1-170.1-147.1-92 0-131.1 64-171.1 147.1l-3 6c-63 122.1-129.1 258.1-200.1 409.2v2l-21 46c-8 19-12 29-13 32-51 140.1 54 263.1 181.1 263.1 1 0 5 0 10-1h14c66-8 134.1-50 203.1-125.1 69 75 137.1 117.1 203.1 125.1h14c5 1 9 1 10 1 127.1.1 232.1-123 181.1-263.1z"></path>
-        </svg>
-        <Text color="gray.600" fontSize="0.875rem" pl="0.5rem">
-          &copy; 2019 company, Inc. All rights reserved.
-        </Text>
+      <Flex maxW="64rem" mx="auto" alignItems="center" px={10} flexDir={'row'} justifyContent={'center'}>
+        <List lineHeight="2" display={'flex'} gap={4} flexDir={'row'} >
+          <LinkItem color={'#000'} fontSize={'10px'} text="Terms of Use" />
+          <LinkItem color={'#000'} fontSize={'10px'} text="Privacy Policy" />
+          <LinkItem color={'#000'} fontSize={'10px'} text="Cookie Policy" />
+          <LinkItem color={'#000'} fontSize={'10px'} text="Do Not Sell My Personal Information" />
+          <LinkItem color={'#000'} fontSize={'10px'} text="Accessibility" />
+          <LinkItem color={'#000'} fontSize={'10px'} text="© 2024 InventOnBTC, Inc. All Rights Reserved" />
+        </List>
       </Flex>
     </Box>
   );
